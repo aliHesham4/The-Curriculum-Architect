7f6a5276db8a64bf5b12e0534ed3058936c15a0b
Ali Hesham Abdelhady <ali04hesham@gmail.com>
2026-06-06 15:51:33 +0300

    removed test file

--- test.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="")
-
-model = genai.GenerativeModel("gemini-2.5-flash")
-response = model.generate_content("Your prompt here")
-print(response.text)
